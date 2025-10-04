db7a7e5a769eaf48fa70a1a8b2e7b24578117be6
Gabriel <gvogt@asu.edu>
2025-10-03 19:55:38 -0700

    feat(GPS): added long, lat and satellites to home page, added a card for the GPS data

--- AeroMQDataHub/Telemetry.cs
+++ AeroMQDataHub/Telemetry.cs
@@ -13,5 +13,10 @@ namespace AeroMQDataHub
         public double Yaw { get; set; }
         public double Volts { get; set; }
         public double Amps { get; set; }
+        public int Fix { get; set; }
+        public int Sats { get; set; }
+        public double Lat { get; set; }
+        public double Lon { get; set; }
+        public double Alt { get; set; }
     }
 }
